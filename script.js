752342fc36a2ffe8ad78379cee228abbb7f2e5eb
Lavanya Agrawal <lavanya.kanak.agrawal@gmail.com>
2026-05-28 19:24:19 +0530

    Added Calculator UI

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("hello");
+window.console.log("hello2");
+
+//console.log(window)
+//console.log("hello2)
+//alert("hello world!");
+
+btn1.onclick = () => {
+    console.log(evt);
+    console.log(evt.type);
+    console.log(evt.target);
+    console.log(evt.currentTarget);
+    console.log(evt.clientX);
+};
+
